831b7c29bb55caa9c5ac2ad9a3d72aa1a65116a2
bpayt2 <brypayty_50@yahoo.com>
2019-04-22 13:35:24 -0500

    Create vars.tf

---
+++ vault/terraform/vault-consul/vars.tf
@@ -0,0 +1,16 @@
+variable vault_public_ip {}
+variable vault_instance_type {
+  default = "t2.micro"
+}
+variable "key_name" {
+  default = "S3FS"
+}
+variable "vault_sg_name" {
+  default = "default"
+}
+variable "region" {
+  default = "us-east-1"
+}
+variable "hosted_zone" {
+  default = "ZVTNY0A2OO1DW"
+}
