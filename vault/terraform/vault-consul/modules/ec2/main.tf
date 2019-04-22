7c6b2bee1af44530966353f2ef1e8af8c6ee993e
bpayt2 <brypayty_50@yahoo.com>
2019-04-22 13:56:35 -0500

    Create main.tf

---
+++ vault/terraform/vault-consul/modules/ec2/main.tf
@@ -0,0 +1,21 @@
+data "aws_ami" "vault-consul" {
+  most_recent = true
+  owners      = ["self"]
+
+  filter {
+    name   = "name"
+    values = ["vault-consul-hashicorop-demo"]
+  }
+}
+
+resource "aws_instance" "vault" {
+  ami                    = "${data.aws_ami.vault-consul.id}"
+  instance_type          = "${var.vault_instance_type}"
+  key_name               = "${var.key_name}"
+  vpc_security_group_ids = ["${var.vault_sg_name}"]
+  user_data = "${data.template_file.user_data_jenkins.rendered}"
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = 30
+    delete_on_termination = false
+  }
