dd5b530fab10bcc2ccdcce927acfb6d5627d8763
bpayt2 <brypayty_50@yahoo.com>
2019-04-22 13:28:34 -0500

    Create vars.tf

---
+++ jenkins/terraform/vars.tf
@@ -0,0 +1,16 @@
+variable "jenkins_master_public_ip" {}
+variable jenkins_master_instance_type {
+  default = "t2.micro"
+}
+variable "key_name" {
+  default = "S3FS"
+}
+variable "master_sg_name" {
+  default = "default"
+}
+variable "region" {
+  default = "us-east-1"
+}
+variable "hosted_zone" {
+  default = "ZVTNY0A2OO1DW"
+}
