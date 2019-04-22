8f910d422b5ec13c4211470677ebbb8b164c06c4
bpayt2 <brypayty_50@yahoo.com>
2019-04-22 14:03:25 -0500

    Update main.tf

--- vault/terraform/vault-consul/main.tf
+++ vault/terraform/vault-consul/main.tf
@@ -11,45 +11,20 @@ terraform {
     }
 }
 
-data "terraform_remote_state""aws_global" {
-    backend = "s3"
-    config {
-            region = "us-east-1"
-            bucket = "hashicorp-demo-state"
-            key = "terraform/jenkins/terraform.tfstate"
+module "ec2" {
+    source = "./modules/ec2"
+    region = "${var.region}"
+    instance_type = "${var.vault_instance_type}"
+    key_name = "${var.key_name}"
+    vpc_security_group_ids = ["${var.vault_sg_name}"]
     }
-}
-data "aws_ami" "vault-consul" {
-  most_recent = true
-  owners      = ["self"]
+ 
+module "r53" {
+    source = "./modules/r53"
+    zone_id = "${var.hosted_zone}"
+    records = ["${module.ec2.vault_public_ip}"]
+    }
 
-  filter {
-    name   = "name"
-    values = ["vault-consul-hashicorop-demo"]
-  }
-}
-
-resource "aws_instance" "vault" {
-  ami                    = "${data.aws_ami.vault-consul.id}"
-  instance_type          = "${var.vault_instance_type}"
-  key_name               = "${var.key_name}"
-  vpc_security_group_ids = ["${var.vault_sg_name}"]
-  root_block_device {
-    volume_type           = "gp2"
-    volume_size           = 30
-    delete_on_termination = false
-  }
-
-  tags {
-    Name   = "vault-consul"
-    Project   = "hashicorp_demo"
-  }
-}
-
-resource "aws_route53_record" "jenkins" {
-  zone_id = "${var.hosted_zone}"
-  name = "hashicorp_demo_vault.bap.com"
-  type = "A"
-  ttl = "300"
-  records = ["${var.vault_public_ip}"]
-}
+ ouput "vault_public_ip" {
+     value = "${module.ec2.vault_public_ip}"
+    }
