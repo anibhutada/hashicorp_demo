f61dc65376119d0470871f8890e7fda70de24826
bpayt2 <brypayty_50@yahoo.com>
2019-04-24 19:47:19 -0500

    Delete main.tf

--- vault/terraform/vault-consul/modules/r53/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_route53_record" "vault" {
-  zone_id = "${var.hosted_zone}"
-  name = "hashicorp_demo_vault.bap.com"
-  type = "A"
-  ttl = "300"
-  records = ["${var.vault_public_ip}"]
-}
